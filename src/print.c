36e0d3a823327f4652e6b3ba9efea34dee6cd1c1
bede-fre <bede-fre@student.42.fr>
2017-12-05 18:14:11 +0100

    ft_display fix

--- src/print.c
+++ src/print.c
@@ -6,7 +6,7 @@
 /*   By: bede-fre <bede-fre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/05 15:59:44 by bede-fre          #+#    #+#             */
-/*   Updated: 2017/12/05 17:46:53 by bede-fre         ###   ########.fr       */
+/*   Updated: 2017/12/05 18:13:34 by bede-fre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,11 @@ void	ft_display(t_map map)
 {
 	char	*tmp;
 
-	tmp = map->grid;
+	tmp = map.grid;
 	while (*tmp != '\0')
 	{
-		write(1, tmp, map->width);
-		tmp = tmp + map->width;
+		write(1, tmp, map.width);
+		tmp = tmp + map.width;
 		ft_putchar('\n');
 	}
 }
