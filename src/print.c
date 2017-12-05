aaba32f0ea15cce59fbd5b6e424beeee89912da2
bede-fre <bede-fre@student.42.fr>
2017-12-05 17:50:30 +0100

    ft_display(should work)

--- src/print.c
+++ src/print.c
@@ -6,10 +6,22 @@
 /*   By: bede-fre <bede-fre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/05 15:59:44 by bede-fre          #+#    #+#             */
-/*   Updated: 2017/12/05 16:00:37 by bede-fre         ###   ########.fr       */
+/*   Updated: 2017/12/05 17:46:53 by bede-fre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
+#include <unistd.h>
 
-void	ft_display(t_tetra *tab, t_map map);
+void	ft_display(t_map map)
+{
+	char	*tmp;
+
+	tmp = map->grid;
+	while (*tmp != '\0')
+	{
+		write(1, tmp, map->width);
+		tmp = tmp + map->width;
+		ft_putchar('\n');
+	}
+}
