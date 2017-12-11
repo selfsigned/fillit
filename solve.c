f878ee7832c84346e8f453492791a2d083cb60ba
xperrin <xperrin@student.42.fr>
2017-12-11 13:41:41 +0100

    More algo helpers progress

--- solve.c
+++ solve.c
@@ -6,7 +6,7 @@
 /*   By: bede-fre <bede-fre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/05 15:58:21 by bede-fre          #+#    #+#             */
-/*   Updated: 2017/12/11 01:26:25 by xperrin          ###   ########.fr       */
+/*   Updated: 2017/12/11 13:41:17 by xperrin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,10 +45,8 @@ static	int		is_placeable(t_tetra tet, t_map  map, int y, int x)
 {
 	int	i;
 	int	j;
-	int	cnt;
 
 	i = -1;
-	cnt = 0;
 	while (++i < 4)
 	{
 		j = -1;
@@ -60,6 +58,26 @@ static	int		is_placeable(t_tetra tet, t_map  map, int y, int x)
 		}
 	}
 	return (TRUE);
+}
+
+static	void	place_on_map(t_tetra tet, t_map map, int y, int x)
+{
+	int	i;
+	int	j;
+	int	cnt;
+
+	i = -1;
+	cnt = 0;
+	while (++i < 4)
+	{
+		j = -1;
+		while (++j < 4 && cnt != 4)
+		{
+			if (ft_isalpha(map.grid[y + i][x + j])
+					&& ft_isalpha(tet.content[i][j]))
+				map.grid[y + i][x + j] = tet.content[i][j];
+		}
+	}
 }
 
 int				ft_algo(t_tetra *tet_in, t_map map, int mapw)
@@ -76,16 +94,17 @@ void		ft_solve(t_tetra *tet_in)
 	t_map	map;
 	int		width;
 
-	width = 2;
+	width = 4;
 	map.width = width;
 	map.is_solved = 0;
 	map.grid = alloc_and_fill(width);
-	while (!ft_algo(tet_in, map, width))
-	{
-		width++;
-		free(map.grid);
-		map.grid = alloc_and_fill(width);
-	}
+	/* while (!ft_algo(tet_in, map, width)) */
+	/* { */
+	/* 	width++; */
+	/* 	free(map.grid); */
+	/* 	map.grid = alloc_and_fill(width); */
+	/* } */
+	place_on_map(tet_in[0], map, 0, 0);
 	ft_display(map.grid, width);
 	ft_strdeltab(map.grid, width + 1);
 }
